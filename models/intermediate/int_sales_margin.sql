8b8805a1c45e587dc7d0ca7a85745fa37cf1fdfa
eden.henninot@gmail.com <eden.henninot@gmail.com>
2024-10-29 14:21:30 +0000

    Join product + sales

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,6 @@
-SELECT *,
-FROM  {{ref("stg_raw__product")}} AS product 
-JOIN {{ref("stg_raw__sales")}} 
+SELECT *
+    , ROUND ((quantity * purchase_price),2) AS purchase_cost
+    , ROUND ((revenue- (quantity * purchase_price)),2) AS margin
+FROM  {{ref("stg_raw__product")}}
+JOIN {{ref("stg_raw__sales")}}
 USING (products_id)
