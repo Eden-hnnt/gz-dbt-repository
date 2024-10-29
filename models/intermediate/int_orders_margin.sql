2657c8ba2372204d5a344a8c77304be48ffb3faf
eden.henninot@gmail.com <eden.henninot@gmail.com>
2024-10-29 14:35:35 +0000

    création de orders_margin.sql

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,9 @@
+SELECT 
+    date_date,
+    orders_id,
+    SUM (revenue) AS tot_revenue,
+    SUM (margin) AS tot_margin,
+    SUM (quantity) AS tot_quantity,
+    SUM (purchase_cost) AS tot_purchase_cost
+FROM {{ref("int_sales_margin")}}
+GROUP BY orders_id, date_date
