d1f81abb95237b2270fb9c50930de285e0b6f32c
eden.henninot@gmail.com <eden.henninot@gmail.com>
2024-10-29 13:17:41 +0000

    creation 2 des modèles

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,13 +11,12 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-
+        shipping_fee_1,
         logcost,
-        CAST (ship_cost AS INT64) ship_cost
-
+        ship_cost
 
     from source
-    WHERE shipping_fee_1 <> shipping_fee
+
 )
 
 select * from renamed
