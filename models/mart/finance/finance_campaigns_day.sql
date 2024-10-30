56c16e627b2463f38dbc0097a267e8368b61f57b
eden.henninot@gmail.com <eden.henninot@gmail.com>
2024-10-30 14:25:59 +0000

    update

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,16 +1,16 @@
 SELECT
-    c.ads_cost,
-    c.ads_impression,
-    c.ads_click,
-    c.date_date,
-    f.numb_orders,
-    f.tot_revenue_day,
-    f.average_basket, 
-    f.tot_op_margin_day,
-    f.tot_purchase_cost_day,
-    f.tot_ship_fee_day,
-    f.tot_logcost_day,
-    f.tot_prod_sold,
+    c.ads_cost AS ads_cost, 
+    c.ads_impression AS ads_impression,
+    c.ads_click AS ads_click,
+    c.date_date AS date_date,
+    f.numb_orders AS numb_orders,
+    f.tot_revenue_day AS tot_revenue_day,
+    f.average_basket AS average_basket, 
+    f.tot_op_margin_day AS tot_op_margin_day,
+    f.tot_purchase_cost_day AS tot_purchase_cost_day,
+    f.tot_ship_fee_day AS tot_ship_fee_day,
+    f.tot_logcost_day AS tot_logcost_day,
+    f.tot_prod_sold AS tot_prod_sold,
     tot_op_margin_day - ads_cost AS ads_margin
  FROM {{ ref('int_campaigns_day') }} c
  FULL OUTER JOIN {{ ref('finance_days') }} f
---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,17 @@
+SELECT *,
+    FORMAT_DATE('%Y-%m',DATE (c.date_date)) AS date_month,
+    ROUND (SUM (ads_margin),1) AS ads_margin_month,
+    ROUND (SUM (ads_cost),1) AS ads_cost_month, 
+    ROUND (SUM (ads_impression),1) AS ads_impression_month, 
+    ROUND (SUM (ads_click),1)  AS ads_click_month,
+    ROUND (SUM (numb_orders),1)  AS numb_orders_month, 
+    ROUND (SUM (tot_revenue_day),1)  AS revenu_month,
+    ROUND (SUM (average_basket),1)  AS average_month,
+    ROUND (SUM (tot_op_margin_day),1)  AS margin_month,
+    ROUND (SUM (tot_purchase_cost_day),1)  AS purchase_cost_month, 
+    ROUND (SUM (tot_ship_fee_day),1)  AS ship_fee_month, 
+    ROUND (SUM (tot_logcost_day),1)  AS logcost_month,
+    ROUND (SUM (tot_prod_sold),1)  AS prod_sold_month
+ FROM {{ ref('finance_campaigns_day') }}
+ GROUP BY datemonth
+ ORDER BY datemonth DESC
