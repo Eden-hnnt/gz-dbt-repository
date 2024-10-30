db44c99be182ffd9bfa36bf8a5991db0fb9eaa17
eden.henninot@gmail.com <eden.henninot@gmail.com>
2024-10-30 14:03:21 +0000

    update

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,19 +1,18 @@
 SELECT
-     date_date,
-     tot_op_margin_day - ads_cost AS ads_margin,
-     ROUND(average_basket,2) AS average_basket,
-     operational_margin,
-     ads_cost,
-     ads_impression,
-     ads_clicks,
-     quantity,
-     revenue,
-     purchase_cost,
-     margin,
-     shipping_fee,
-     logcost,
-     ship_cost
- FROM {{ ref('int_campaigns_day') }}
- FULL OUTER JOIN {{ ref('finance_days') }}
+    c.ads_cost,
+    c.ads_impression,
+    c.ads_click,
+    c.date_date,
+    f.numb_orders,
+    f.tot_revenue_day,
+    f.average_basket, 
+    f.tot_op_margin_day,
+    f.tot_purchase_cost_day,
+    f.tot_ship_fee_day,
+    f.tot_logcost_day,
+    f.tot_prod_sold,
+    tot_op_margin_day - ads_cost AS ads_margin
+ FROM {{ ref('int_campaigns_day') }} c
+ FULL OUTER JOIN {{ ref('finance_days') }} f
      USING (date_date)
  ORDER BY date_date DESC
